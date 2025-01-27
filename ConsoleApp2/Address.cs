f269c6d3390c921f668f6c166e88c86bdf835532
Никита <153456515+Lairont@users.noreply.github.com>
2025-01-27 02:47:01 +0200

    Add files via upload

---
+++ ConsoleApp2/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Address
+    {
+        public string Index;
+        public string Country;
+        public string City;
+        public string Street;
+        public int House;
+        public int Apartment;
+        public void AddressInfo()
+        {
+            Console.WriteLine($"Почтовый индекс: {Index}");
+            Console.WriteLine($"Страна: {Country}");
+            Console.WriteLine($"Город: {City}");
+            Console.WriteLine($"Улица: {Street}");
+            Console.WriteLine($"Дом: {House}");
+            Console.WriteLine($"Квартира: {Apartment}");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// Задание 2. Создать класс с именем Address. В теле класса требуется создать поля: index, country, city, street, house, apartаment.
+    /// Создать экземпляр класса Address.В поля экземпляра записать информацию о почтовом адресе.
+    ///  Выведите на экран значения полей, описывающих адрес.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Index = "2000";
+            address.Country = "Молдова";
+            address.City = "Кишинёв";
+            address.Street = "Бульвар Гагарина";
+            address.House = 757;
+            address.Apartment = 56;
+            address.AddressInfo();
+            Console.ReadLine();
+        }
+    }
+}
