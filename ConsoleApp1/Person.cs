bf988b03dc46ad8ad5d87b1aa6a04eb6402be4c8
Никита <153456515+Lairont@users.noreply.github.com>
2025-01-27 02:28:39 +0200

    Add files via upload

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public byte Age;
+        public void Info()
+        {
+            Console.WriteLine($"Имя: {FirstName}");
+            Console.WriteLine($"Фамилия: {LastName}");
+            Console.WriteLine($"Возраст: {Age}");
+
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Задание 1. Создать класс Person с полями: имя, фамилия, возраст и методом вывода всех данных на консоль.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Никита";
+            person.LastName = "Банчу";
+            person.Age = 17;
+            person.Info();
+            Console.ReadLine();
+        }
+    }
+}
